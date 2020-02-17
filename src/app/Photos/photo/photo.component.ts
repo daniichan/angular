168afc6fd79fae95aca76d4fd2e0fa5898108531
Daniele Leite <danii.exe@gmail.com>
2020-02-17 14:08:08 -0300

    Aula 2

---
+++ src/app/Photos/photo/photo.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-photo',
+  templateUrl: './photo.component.html'
+})
+export class PhotoComponent implements OnInit {
+
+  @Input() url = "";
+  @Input() description = "";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
