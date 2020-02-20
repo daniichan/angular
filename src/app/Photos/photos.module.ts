3e4886ac27f543877cd64cd69f8fb26b038be913
Daniele Leite <danii.exe@gmail.com>
2020-02-20 16:00:13 -0300

    Aula 7

--- src/app/Photos/photos.module.ts
+++ src/app/Photos/photos.module.ts
@@ -1,26 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 
-import { PhotoComponent } from './photo/photo.component';
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotoRowComponent } from './photo-list/photo-row/photo-row.component';
-import { FilterByDescription } from './photo-list/filter-by-description.pipe';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { PhotoListModule } from './photo-list/photo-list.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoModule } from './photo/photo.module';
 
 @NgModule({
-    declarations: [ 
-        PhotoComponent, 
-        PhotoListComponent, 
-        PhotoFormComponent, 
-        PhotoRowComponent,
-        FilterByDescription,
-        LoadButtonComponent 
-    ],
     imports: [ 
-        CommonModule,
-        HttpClientModule
+        PhotoModule,
+        PhotoFormModule,
+        PhotoListModule,
     ]
 })
 export class PhotosModule { }
