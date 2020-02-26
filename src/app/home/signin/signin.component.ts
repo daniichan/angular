00d4569ee75628a22ff9d57f42876ac5ba849522
Daniele Leite <danii.exe@gmail.com>
2020-02-26 11:03:30 -0300

    Angular 2. Aula 1 - 12

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
-import { AuthService } from './../../core/auth.service';
+import { AuthService } from '../../core/auth/auth.service';
+import { PlatformDetectorService } from './../../core/platform-detector/platform-detector.service';
 
 @Component({
     templateUrl: './signin.component.html'
@@ -15,7 +16,8 @@ export class SignInComponent implements OnInit {
     constructor(
         private formBuilder: FormBuilder,
         private authService: AuthService,
-        private router: Router
+        private router: Router,
+        private platformDetectorService: PlatformDetectorService
     ) { }
 
     ngOnInit() {
@@ -35,7 +37,8 @@ export class SignInComponent implements OnInit {
                 () => this.router.navigate(['user', userName]),
                 err => {
                     this.loginForm.reset();
-                    this.userNameInput.nativeElement.focus();
+                    this.platformDetectorService.isPlatformBrowser() && 
+                        this.userNameInput.nativeElement.focus();
                 }
             );
     }
