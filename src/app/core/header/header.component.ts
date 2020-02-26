4229178c904b2ce9057981f27bc2b8271df99241
Daniele Leite <danii.exe@gmail.com>
2020-02-26 16:51:25 -0300

    Angular 2. Aula 3 - 7

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 import { UserService } from './../user/user.service';
 import { User } from '../user/user';
@@ -12,7 +13,15 @@ export class HeaderComponent {
     
     user$: Observable<User>;
 
-    constructor(userService: UserService) { 
+    constructor(
+        private userService: UserService,
+        private router: Router
+    ) { 
         this.user$ = userService.getUser();
+    }
+
+    logout() {
+        this.userService.logout();
+        this.router.navigate(['']);
     }
 }
