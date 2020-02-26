98fb0d4cc7f35f6118217b8995f46f860d35fb80
Daniele Leite <danii.exe@gmail.com>
2020-02-26 10:16:46 -0300

    Angular 2. Aula 1 - 6/7

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+import { AuthService } from './../../core/auth.service';
+
 @Component({
     templateUrl: './signin.component.html'
 })
@@ -8,12 +10,30 @@ export class SignInComponent implements OnInit {
 
     loginForm: FormGroup;
 
-    constructor(private formBuilder: FormBuilder) { }
+    constructor(
+        private formBuilder: FormBuilder,
+        private authService: AuthService
+    ) { }
 
     ngOnInit() {
         this.loginForm = this.formBuilder.group({
             userName: ['', Validators.required],
             password: ['', Validators.required]
         });
+    }
+
+    login() {
+        const userName = this.loginForm.get('userName').value;
+        const password = this.loginForm.get('password').value;
+
+        this.authService
+            .authenticate(userName, password)
+            .subscribe( 
+                () => console.log('ok'), 
+                err => {
+                    console.log(err);
+                    this.loginForm.reset();
+                }
+            );
     }
 }
