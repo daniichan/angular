51580867b00e4ae04f6ae664174e42f0b926dbd8
Daniele Leite <danii.exe@gmail.com>
2020-02-28 12:53:18 -0300

    Angular 2. Aula 5 - 7

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './header/header.component';
+import { PlatformDetectorService } from './platform-detector/platform-detector.service';
 
 @NgModule({
     declarations: [
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -8,6 +8,8 @@ import { VMessageModule } from './../shared/components/vmessage/vmessage.module'
 import { SignUpComponent } from './signup/signup.component';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home.routing.module';
+import { SignUpService } from './signup/signup.service';
+import { CoreModule } from './../core/core.module';
 
 @NgModule({
     declarations: [
@@ -21,7 +23,10 @@ import { HomeRoutingModule } from './home.routing.module';
         ReactiveFormsModule,
         VMessageModule,
         RouterModule,
-        HomeRoutingModule
+        HomeRoutingModule,
+    ],
+    providers: [
+        SignUpService,
     ]
 })
 export class HomeModule { }
