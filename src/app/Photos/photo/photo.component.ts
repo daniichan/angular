daad6a0f48c446042ad3366c05f061c073038178
Daniele Leite <danii.exe@gmail.com>
2020-03-02 10:39:05 -0300

    Angular 3. Aula 1 - 11

--- src/app/Photos/photo/photo.component.ts
+++ src/app/Photos/photo/photo.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+const cloud = 'http://localhost:3000/imgs/';
+
 @Component({
   selector: 'app-photo',
   templateUrl: './photo.component.html'
 })
-export class PhotoComponent implements OnInit {
+export class PhotoComponent {
 
-  @Input() url = "";
+  private _url = '';
+
   @Input() description = "";
 
-  constructor() { }
+  // @Input() url = "";
+  @Input() set url(url: string) {
+    if (!url.startsWith('data')) {
+      this._url = cloud + url;
+    } else {
+      this._url = url;
+    }
+  }
 
-  ngOnInit() {
+  get url() {
+    return this._url;
   }
 
 }
