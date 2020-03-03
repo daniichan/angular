0e4e653846b03a43f837ac4f2e4c1240ff1b83b7
Daniele Leite <danii.exe@gmail.com>
2020-03-03 12:46:36 -0300

    Angular 3. Aula 4 - 7

--- src/app/Photos/photo-details/photo-comments/photo-comments.component.ts
+++ src/app/Photos/photo-details/photo-comments/photo-comments.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Observable } from 'rxjs';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { switchMap, tap  } from 'rxjs/operators'
 
 import { PhotoComment } from '../../photo/photo-comment';
 import { PhotoService } from '../../photo/photo.service';
@@ -30,11 +31,11 @@ export class PhotoCommentsComponent implements OnInit {
 
     save() {
         const comment = this.commentForm.get('comment').value as string;
-        this.photoService
+        this.comments$ = this.photoService
             .addComment(this.photoId, comment)
-            .subscribe(() => {
+            .pipe(switchMap(() => this.photoService.getComments(this.photoId)))
+            .pipe(tap(() => {
                 this.commentForm.reset();
-                alert('Comentário adicionado com sucesso.');
-            });
+            }));
     }
 }
