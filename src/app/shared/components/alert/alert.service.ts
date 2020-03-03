26960634296c5116c7be2315fad6ef8a8f811126
Daniele Leite <danii.exe@gmail.com>
2020-03-03 16:40:15 -0300

    Angular 3. Aula 6 - 4

--- src/app/shared/components/alert/alert.service.ts
+++ src/app/shared/components/alert/alert.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+import { Alert, AlertType } from './alert';
 
 @Injectable({ providedIn: 'root' })
-export class AlertService { }
+export class AlertService { 
+
+    alertSubject: Subject<Alert>;
+
+    private alert(alertType: AlertType, message: string) {
+        this.alertSubject.next(new Alert(alertType, message));
+    }
+
+    getAlert() {
+        return this.alertSubject.asObservable();
+    }
+
+    success(message:string) {
+        this.alert(AlertType.SUCCESS, message);
+    }
+
+    warning(message: string) {
+        this.alert(AlertType.WARNING, message);
+    }
+
+    danger(message: string) {
+        this.alert(AlertType.DANGER, message);
+    }
+
+    info(message: string) {
+        this.alert(AlertType.INFO, message);
+    }
+}
