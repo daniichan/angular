0fb6718ae0824dc824721d3edbe766e0b7a1b38f
Daniele Leite <danii.exe@gmail.com>
2020-03-03 16:59:26 -0300

    Angular 3. Aula 6 - 7

--- src/app/Photos/photo-details/photo-details.component.ts
+++ src/app/Photos/photo-details/photo-details.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 
 import { PhotoService } from './../photo/photo.service';
 import { Photo } from '../photo/photo';
+import { AlertService } from './../../shared/components/alert/alert.service';
 
 @Component({
     templateUrl: './photo-details.component.html'
@@ -16,7 +17,8 @@ export class PhotoDetailsComponent implements OnInit {
     constructor(
         private route: ActivatedRoute,
         private photoService: PhotoService,
-        private router: Router
+        private router: Router,
+        private alertService: AlertService
     ) {}
 
     ngOnInit() {
@@ -27,6 +29,14 @@ export class PhotoDetailsComponent implements OnInit {
     remove() {
         this.photoService
             .removePhoto(this.photoId)
-            .subscribe(() => this.router.navigate(['']));
+            .subscribe(() => {
+                this.alertService.success('Photo removed.');
+                this.router.navigate(['']);
+            },
+                err => {
+                    console.log('error: ', err);
+                    this.alertService.warning("Couldn't delete the photo.");
+            }
+            );
     }
 }
