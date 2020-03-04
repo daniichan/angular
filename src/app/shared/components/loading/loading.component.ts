018a20e94a2e61f1f0441143025c183c9711e921
Daniele Leite <danii.exe@gmail.com>
2020-03-04 15:42:05 -0300

    Angular 4. Aula 3 - 4

--- src/app/shared/components/loading/loading.component.ts
+++ src/app/shared/components/loading/loading.component.ts
@@ -7,7 +7,7 @@ import { map } from 'rxjs/operators';
 @Component({
     selector: 'app-loading',
     templateUrl: './loading.component.html',
-    styleUrls: ['./loading.component.scss']
+    styleUrls: ['loading.component.scss']
 })
 export class LoadingComponent implements OnInit {
 
