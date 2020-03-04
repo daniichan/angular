cbc0567dbc0ee79441a53d753a9ac125578e293d
Daniele Leite <danii.exe@gmail.com>
2020-03-04 15:37:20 -0300

    Angular 4. Aula 3 - 3

--- src/app/shared/components/loading/loading.component.ts
+++ src/app/shared/components/loading/loading.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { LoadingService } from './loading.service';
+import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-loading',
     templateUrl: './loading.component.html',
     styleUrls: ['./loading.component.scss']
 })
-export class LoadingComponent { }
+export class LoadingComponent implements OnInit {
+
+    loading$: Observable<string>;
+
+    constructor(private loadingService: LoadingService) {}
+
+    ngOnInit() {
+        this.loading$ = this.loadingService
+            .getLoading()
+            .pipe(map(loadingType => loadingType.valueOf()));
+    }
+}
