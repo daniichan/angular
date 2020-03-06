9de32a1a6c90e3ffb9ddfef1bc66ff7eaac90b90
Daniele Leite <danii.exe@gmail.com>
2020-03-06 12:59:59 -0300

    Angular 4. Aula 5 - 6

--- src/app/errors/global-error-handler/global-error-handler.ts
+++ src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,9 +1,30 @@
-import { ErrorHandler } from '@angular/core';
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import * as StackTrace from 'stacktrace-js';
 
-export class GlobalErrorHandler implements ErrorHandler {
+import { UserService } from './../../core/user/user.service';
 
+@Injectable()
+export class GlobalErrorHandler implements  ErrorHandler {
+
+    constructor(private injector: Injector) {}
+    
     handleError(error: any): void {
-        throw error;
-    }
+        const location = this.injector.get(LocationStrategy);
+        const url = location instanceof PathLocationStrategy ? location.path() : '';
+        const message = error.message ? error.message : error.toString(); 
+        const userService = this.injector.get(UserService);
 
+        StackTrace
+            .fromError(error)
+            .then(stackFrames => {
+                const stackAsString = stackFrames
+                    .map(sf => sf.toString())
+                    .join('\n');
+
+                console.log(message);
+                console.log(StackTrace);
+                console.log({ message, url, userName: userService.getUserName(), stack: stackAsString })
+            })
+    }
 }
